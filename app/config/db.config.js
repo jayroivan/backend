51e4e1e5e06b2623bc7a1806ae214fe054ebc5d6
jayroivan <ma0721032017@unab.edu.sv>
2020-06-26 17:26:02 -0600

    second2 commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/saturno",
+    url: "mongodb+srv://dbJayro:jivan.99@saturno-hwnge.mongodb.net/saturno?retryWrites=true&w=majority",
     JWT_KEY: "dsadasd4sa65d4as56d46as5d46as5d46a5s",
   };
